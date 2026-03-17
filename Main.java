77d0cee3b866c46705b3265b05e2cb747759c101
ZLX666888999 <2182253105@qq.com>
2026-03-17 15:34:19 +0800

    123456

--- Main.java
+++ Main.java
@@ -1,12 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        String sent1 = "I was very satisfied with the service.";
-        String sent2 = "The e-Bike is quite comfortable to ride.";
-        String sent3 = "The battery life of the e-Bike is impressive.";
-        String sent4 = "The customer support was helpful and responsive.";
-        String sent5 = "I would recommend this e-Bike to my friends and family.";
-        Feedback userFeedback = new Feedback("John", "Doe", "john.doe@example.com");
-        userFeedback.analyseFeedback(false, sent1, sent2, sent3, sent4, sent5);
-        System.out.println(userFeedback);
+        UserRegistration userReg = new UserRegistration();
+        userReg.registration();
+        System.out.println(userReg);
     }
 }
